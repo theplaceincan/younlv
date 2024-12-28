b799f1527ac37ad88e911eb57c8218df41cd5c9a
me <me@gmail.com>
2024-12-27 19:06:56 -0800

    fixed button locations in gpacalculator, added load json file function & parsing ability

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -82,7 +82,7 @@ onUnmounted(() => {
     </div>
     <div class="flex items-center space-x-2">
       <!-- Username/menu -->
-       <!-- <a href="/menu"><button v-if="isAuthenticated" :class="`font-semibold textLink theme-${websiteTheme} text-secondaryText`">{{model.username}}</button></a> -->
+      <!-- <a href="/menu"><button v-if="isAuthenticated" :class="`font-semibold textLink theme-${websiteTheme} text-secondaryText`">{{model.username}}</button></a> -->
       <!-- Theme toggle -->
       <button
         class="font-semibold text-white rounded-full p-1 text-sm bg-unlvRed hover:bg-unlvDarkRed active:bg-unlvDarkerRed transition ease-in-out"
@@ -104,9 +104,14 @@ onUnmounted(() => {
       </button>
       <!-- Profile button -->
       <a :href="isAuthenticated ? '/menu' : '/register'">
-        <button :class="{ 'bg-unlvGray': isAuthenticated }"
-          class="font-semibold text-white rounded-full p-1 text-sm bg-unlvRed hover:bg-unlvDarkRed active:bg-unlvDarkerRed transition ease-in-out">
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+        <button :href="isAuthenticated ? '/menu' : '/register'" :class="isAuthenticated ? 'bg-unlvGray' : 'bg-unlvRed'"
+          class="font-semibold text-white rounded-full p-1 text-sm hover:bg-unlvDarkRed active:bg-unlvDarkerRed transition ease-in-out">
+          <svg v-if="!isAuthenticated" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+            stroke="currentColor" class="size-6">
+            <path stroke-linecap="round" stroke-linejoin="round"
+              d="M15.75 9V5.25A2.25 2.25 0 0 0 13.5 3h-6a2.25 2.25 0 0 0-2.25 2.25v13.5A2.25 2.25 0 0 0 7.5 21h6a2.25 2.25 0 0 0 2.25-2.25V15M12 9l-3 3m0 0 3 3m-3-3h12.75" />
+          </svg>
+          <svg v-if="isAuthenticated" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
             stroke="currentColor" class="size-6">
             <path stroke-linecap="round" stroke-linejoin="round"
               d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z" />
--- src/toolpages/gpacalculator.vue
+++ src/toolpages/gpacalculator.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ref, computed } from "vue";
+import { ref, computed, onMounted } from "vue";
 import { useRoute, useRouter } from "vue-router";
 import pb from "../composables/usePocketbase";
 import { websiteTheme } from "../globalvariables";
@@ -8,23 +8,74 @@ const model = computed(() => pb.authStore.model);
 const route = useRoute();
 const router = useRouter();
 
-const isAuthenticated = computed(() => {
-  return pb.authStore.model !== null;
-});
+const isAuthenticated = ref(pb.authStore.model !== null);
 
 let gpaGradePoints = ref(170.200);
 let gpaUnitsTaken = ref(51.000);
 let gpa = ref(3.937);
 
-let semesterSeason = ref("");
-let semesterYear = ref();
+let newSemesterSeason = ref("");
+let newSemesterYear = ref();
 let SemesterSeasonOptions = ["Season", "Spring", "Summer", "Fall"]
-let courseGrade = ref("");
-let courseName = ref("");
-let courseUnits = ref("");
+let newCourseGrade = ref("");
+let newCourseName = ref("");
+let newCourseUnits = ref("");
 
+let coursesJSONFile = ref();
+let coursesJSONURL = ref();
+let courses = ref(
+  {
+    "semesters": [
+      {
+        "year": "",
+        "season": "",
+        "courses": []
+      }
+    ]
+  }
+);
 
 let aa = 1
+
+// Button functions
+
+function deleteSemester() { console.log("Semester deleted") }
+function createSemester() { console.log("Semester created") }
+function deleteCourse() { console.log("Course deleted") }
+function createCourse() { console.log("Course created") }
+function clearSchedule() { console.log("Entire schedule has been cleared") }
+function calculateGPAInfo() { console.log("GPA Info Calculated") }
+function downloadCoursesJSON() { console.log("JSON file of courses downloaded.") }
+
+function loadCoursesJSONfile(event) {
+  coursesJSONFile.value = Array.from(event.target.files)[0];
+  coursesJSONURL.value = URL.createObjectURL(coursesJSONFile.value);
+
+  const reader = new FileReader(); // Create a new FileReader instance
+  reader.onload = function (e) {
+    try {
+      const jsonContent = JSON.parse(e.target.result); // Parse JSON content
+      console.log("JSON courses file loaded.");
+      courses.value = jsonContent;
+      // console.log(courses.value.semesters[1].year);
+      // console.log(courses.value.semesters[1].season);
+      // console.log(courses.value.semesters[1].courses[1]);
+
+    } catch (parseError) {
+      console.error("Error parsing JSON:", parseError.message);
+    }
+  };
+  reader.readAsText(coursesJSONFile.value);
+}
+
+
+onMounted(() => {
+
+  // Updates isAuthenticated automatically
+  pb.authStore.onChange(() => {
+    isAuthenticated.value = pb.authStore.model !== null;
+  });
+});
 </script>
 
 <!-- :class="`theme-${websiteTheme} text-primaryText`" -->
@@ -36,45 +87,91 @@ let aa = 1
     <p :class="`theme-${websiteTheme} text-primaryText`" class="font-semibold underline">Results</p>
     <div>
       <p :class="`theme-${websiteTheme} text-primaryText`">GPA: <span class="font-bold">{{ gpa }}</span> </p>
-      <p :class="`theme-${websiteTheme} text-primaryText`">Units Taken: <span class="font-bold">{{ gpaUnitsTaken }}</span> </p>
-      <p :class="`theme-${websiteTheme} text-primaryText`">Grade Points: <span class="font-bold">{{ gpaGradePoints }}</span> </p>
+      <p :class="`theme-${websiteTheme} text-primaryText`">Units Taken: <span class="font-bold">{{ gpaUnitsTaken
+          }}</span> </p>
+      <p :class="`theme-${websiteTheme} text-primaryText`">Grade Points: <span class="font-bold">{{ gpaGradePoints
+          }}</span> </p>
       <div class="flex flex-col mt-2 items-start space-y-1 justify-center">
-        <button class="rounded-sm w-[100px] bg-blue-600 hover:bg-blue-700 active:bg-blue-800 transition ease-in-out text-white font-semibold">Calculate</button>
-        <button class="rounded-sm w-[100px] bg-lime-600 hover:bg-lime-700 active:bg-lime-800 transition ease-in-out text-white font-semibold">Download</button>
+        <button @click="calculateGPAInfo()"
+          class="rounded-sm w-[190px] bg-blue-600 hover:bg-blue-700 active:bg-blue-800 transition ease-in-out text-white font-semibold">Calculate</button>
+        <button @click="downloadCoursesJSON()"
+          class="rounded-sm w-[190px] bg-lime-600 hover:bg-lime-700 active:bg-lime-800 transition ease-in-out text-white font-semibold">Download
+          JSON file</button>
+        <div class="py-2">
+          <p :class="`theme-${websiteTheme} text-primaryText font-semibold`">Load Courses JSON file</p>
+          <input @change="loadCoursesJSONfile" placeholder="Load JSON file"
+            :class="`theme-${websiteTheme} text-primaryText placeholder-tertiaryText`" type="file">
+        </div>
+        <div class="">
+          <p :class="`theme-${websiteTheme} text-primaryText font-semibold`">How this works</p>
+          <p :class="`theme-${websiteTheme} text-secondaryText`">
+            List your courses and semesters below, then click "Calculate" to compute your GPA. Save your schedule as a
+            JSON file (a type of text file) to continue later by uploading the file ("Choose File" button), editing, and
+            recalculating.
+          </p>
+        </div>
       </div>
     </div>
     <div class="border-t-[1px] my-4 border-tertiary"></div>
     <div>
-      <div v-for="a in aa" class="flex my-5 flex-col border border-tertiary p-2 rounded-sm shadow-md">
+      <div v-for="a in aa" class="flex my-2 flex-col border border-tertiary p-2 rounded-sm shadow-md">
         <div :class="`theme-${websiteTheme} text-secondaryText font-semibold`" class="flex flex-col">
-          <input :class="`hover:bg-secondary transition ease-in-out theme-${websiteTheme} text-primaryText placeholder-tertiaryText`" v-model="semesterSeason" placeholder="Semester name">
-          <input :class="`hover:bg-secondary transition ease-in-out theme-${websiteTheme} text-primaryText placeholder-tertiaryText`" v-model="semesterYear" placeholder="Semester year">
+          <input
+            :class="`hover:bg-secondary transition ease-in-out theme-${websiteTheme} text-primaryText placeholder-tertiaryText`"
+            v-model="newSemesterSeason" placeholder="Semester name">
+          <input
+            :class="`hover:bg-secondary transition ease-in-out theme-${websiteTheme} text-primaryText placeholder-tertiaryText`"
+            v-model="newSemesterYear" placeholder="Semester year">
         </div>
         <div class="border-t-[1px] rounded-sm my-2 border-tertiary"></div>
         <div class="flex flex-wrap">
-          <div v-for="a in aa" :class="`theme-${websiteTheme} border-2 border-tertiary`" class="flex m-1 flex-col w-fit p-1 rounded-sm">
+          <div v-for="a in aa" :class="`theme-${websiteTheme} border-2 border-tertiary`"
+            class="flex m-1 flex-col w-fit p-1 rounded-sm">
             <div class="flex flex-col space-y-1">
-              <input :class="`courseInput border-[2px] border-tertiary theme-${websiteTheme} text-primaryText placeholder-tertiaryText`" v-model="courseName" placeholder="Course name">
-              <input :class="`courseInput border-[2px] border-tertiary theme-${websiteTheme} text-primaryText placeholder-tertiaryText`" v-model="courseGrade" placeholder="Course grade">
-              <input :class="`courseInput border-[2px] border-tertiary theme-${websiteTheme} text-primaryText placeholder-tertiaryText`" v-model="courseUnits" placeholder="Course units">
+              <input
+                :class="`courseInput border-[1px] border-tertiary theme-${websiteTheme} text-primaryText placeholder-tertiaryText`"
+                v-model="newCourseName" placeholder="Course name">
+              <input
+                :class="`courseInput border-[1px] border-tertiary theme-${websiteTheme} text-primaryText placeholder-tertiaryText`"
+                v-model="newCourseGrade" placeholder="Course grade">
+              <input
+                :class="`courseInput border-[1px] border-tertiary theme-${websiteTheme} text-primaryText placeholder-tertiaryText`"
+                v-model="newCourseUnits" placeholder="Course units">
             </div>
             <div class="flex flex-col space-y-1 mt-4 items-center justify-center">
-              <button class="rounded-sm w-full bg-green-600 hover:bg-green-700 active:bg-green-800 transition ease-in-out text-white font-semibold">Create course</button>
-              <button class="rounded-sm w-full bg-orange-600 hover:bg-orange-700 active:bg-orange-800 transition ease-in-out text-white font-semibold">Delete course</button>
+              <button @click="deleteCourse()"
+                class="rounded-sm w-full bg-orange-600 hover:bg-orange-700 active:bg-orange-800 transition ease-in-out text-white font-semibold">Delete
+                course</button>
             </div>
           </div>
         </div>
         <div class="flex flex-col mt-4 space-y-1 items-start justify-center">
-          <button class="rounded-sm px-5 bg-teal-600 hover:bg-teal-700 active:bg-teal-800 transition ease-in-out text-white font-semibold">Create semester</button>
-          <button class="rounded-sm px-5 bg-red-600 hover:bg-red-700 active:bg-red-800 transition ease-in-out text-white font-semibold">Delete semester</button>
+          <button @click="createCourse()"
+            class="rounded-sm px-5 w-[170px] bg-green-600 hover:bg-green-700 active:bg-green-800 transition ease-in-out text-white font-semibold">Create
+            course</button>
+          <button @click="deleteSemester()"
+            class="rounded-sm px-5 w-[170px] bg-red-600 hover:bg-red-700 active:bg-red-800 transition ease-in-out text-white font-semibold">Delete
+            semester</button>
         </div>
       </div>
+      <div class="flex flex-col mt-4 space-y-1 items-start justify-center">
+        <button @click=createSemester()
+          class="rounded-sm px-5 bg-teal-600 hover:bg-teal-700 active:bg-teal-800 transition ease-in-out text-white font-semibold">Create
+          semester</button>
+      </div>
     </div>
-    <button class="rounded-sm w-[100px] bg-red-600 hover:bg-red-700 active:bg-red-800 transition ease-in-out text-white font-semibold">Clear all</button>
+    <div class="border-t-[1px] my-4 border-tertiary"></div>
+    <button @click="clearSchedule()"
+      class="rounded-sm w-[100px] bg-red-600 hover:bg-red-700 active:bg-red-800 transition ease-in-out text-white font-semibold">Clear
+      all</button>
   </div>
 </template>
 
 <style scoped>
+.inputError {
+  background-color: rgba(255, 0, 0, 0.329);
+}
+
 input {
   outline: none;
   background: none;
@@ -86,18 +183,17 @@ select {
 }
 
 .courseInput {
-  padding: 4px;
+  padding: 2px;
   padding-left: 4px;
   border-radius: 0.175rem;
   transition: border 0.2s ease;
 }
 
 .courseInput:hover {
-  border: 2px solid var(--color-unlvRed);
+  border: 1px solid var(--color-unlvRed);
 }
 
 .courseInput:focus {
-  border: 2px solid var(--color-unlvDarkRed);
+  border: 1px solid var(--color-unlvDarkRed);
 }
-
 </style>
